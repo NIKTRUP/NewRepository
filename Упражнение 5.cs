99449739d1b17e33d9bbf2731f9cdb8650db11e2
NIKTRUP <72292425+NIKTRUP@users.noreply.github.com>
2020-12-09 11:21:56 +0300

    Add files via upload

---
+++ Упражнение 5.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Упражнение_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double t1=0,t2=0;
+
+            Console.WriteLine("Введите коэффициент a :");
+            var a1 = Console.ReadLine();
+            Console.WriteLine("Введите коэффициент b :");
+            var b1 = Console.ReadLine();
+            Console.WriteLine("Введите коэффициент c :");
+            var c1 = Console.ReadLine();
+            while (true) 
+            {
+                //проверка
+                if ((int.TryParse(a1, out int a)) && (int.TryParse(b1, out int b)) && (int.TryParse(c1, out int c)))
+                {
+                    //Проверка и повторный ввод корректного коэф. a 
+                    while (a == 0)
+                    {
+                        Console.WriteLine("a не может быть равным нулю в биквадратном уравнении ...");
+                        Console.WriteLine("Введите коэффициент a :");
+                        try
+                        {
+                            a = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Введите коэффициент корректно ");
+                        }
+                    }
+                    // Решение
+                    double D = b * b - 4 * a * c;
+                    if (D == 0)
+                    {
+                        t1 = -b / (2 * a);
+                        if (t1 < 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Действительных корней нет... ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"x1 = {Math.Sqrt(t1)} и x2 = {-Math.Sqrt(t1)}  ");
+                            Console.ResetColor();
+                        }
+                        return;
+                    }
+                    else if (D < 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Действительных корней нет... ");
+                        Console.ResetColor();
+                        return;
+                    }
+                    else
+                    {
+                        t1 = (-b + Math.Sqrt(D)) / 2 * a;
+                        t2 = (-b - Math.Sqrt(D)) / 2 * a;
+                    }
+                    if (t1 < 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("У x1 и x2 нет действительных корней ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"x1 = {Math.Sqrt(t1)} и x2 = {-Math.Sqrt(t1)}  ");
+                        Console.ResetColor();
+                    }
+                    if (t2 < 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("У x3 и x4 нет действительных корней ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"x3 = {Math.Sqrt(t2)} и x4 = {-Math.Sqrt(t2)}  ");
+                        Console.ResetColor();
+                    }
+                    return;
+                }
+                else // чтобы можно было вводить коэффициенты снова
+                {
+                    Console.WriteLine("Введите коэффициенты корректно ");
+                    try
+                    {
+                        Console.WriteLine("Введите коэффициент a :");
+                        a1 = Console.ReadLine();
+                        Console.WriteLine("Введите коэффициент b :");
+                        b1 = Console.ReadLine();
+                        Console.WriteLine("Введите коэффициент c :");
+                        c1 = Console.ReadLine();
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Введите коэффициенты корректно ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Упражнение 6.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Упражнение_6
+{
+    abstract class GeometricFigure
+    {
+       abstract protected double sFigure();
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+    class Rectangle : GeometricFigure
+    {
+        double width , height;
+        public override string ToString()
+        {
+            Rectangle r = new Rectangle(width ,height);
+            return $"Square: {r.sFigure()}\nwidth: {width}\nheight: {height} ";
+        }
+        public double Width
+        {
+            get 
+            {
+                return width;
+            }
+            set 
+            {
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        protected override double sFigure()
+        {
+            return width * height;
+        }
+    }
+    class Quadrate : GeometricFigure
+    {
+        double sidelength;
+        public override string ToString()
+        {
+            Quadrate q = new Quadrate(sidelength);
+            return $"Square: {q.sFigure()}\nsidelength: {sidelength}";
+        }
+        public Quadrate(double sidelength)
+        {
+            this.sidelength = sidelength;
+        }
+        protected override double sFigure()
+        {
+            return sidelength * sidelength;
+        }
+    }
+    class Сircle : GeometricFigure
+    {
+        double radius;
+        const double pi = 3.14;
+
+        public override string ToString()
+        {
+            Сircle c = new Сircle(radius);
+            return $"Square: {c.sFigure()}\nradius: {radius}";
+        }
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public Сircle(double radius)
+        {
+            this.radius = radius;
+        }
+        protected override double sFigure()
+        {
+            return pi * radius * radius;
+        }
+    }
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите ширину прямоугольника");
+            var width1 = Console.ReadLine();
+            Console.WriteLine("Введите высоту прямоугольника");
+            var height1 = Console.ReadLine();
+            Console.WriteLine();
+            if (double.TryParse(width1, out double width) && double.TryParse(height1, out double height))
+            {
+                Rectangle rectangle = new Rectangle(width, height);
+                Console.WriteLine(rectangle);
+                Console.WriteLine();
+               // rectangle.Print();
+            } else Console.WriteLine("Попробуйте ещё раз");
+
+            Console.WriteLine("Введите длину стороны квадрата");
+            var sidelength1 = Console.ReadLine();
+            Console.WriteLine();
+            if (double.TryParse(sidelength1, out double sidelength))
+            {
+                Quadrate quadrate = new Quadrate(sidelength);
+                Console.WriteLine(quadrate);
+                Console.WriteLine();
+              //  quadrate.Print();
+            }
+            else Console.WriteLine("Попробуйте ещё раз");
+
+            Console.WriteLine("Введите радиус круга");
+            var radius1 = Console.ReadLine();
+            Console.WriteLine();
+            if (double.TryParse(radius1, out double radius))
+            {
+                Сircle circle= new Сircle(radius);
+                Console.WriteLine(circle);
+                Console.WriteLine();
+              //  circle.Print();
+            }
+            else Console.WriteLine("Попробуйте ещё раз");
+        }
+    }
+}
